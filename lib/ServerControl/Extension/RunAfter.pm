7e8c5b0002af9a27407b4a8165e665c7cabf1bcf
Jan Gehring <krimdomu@googlemail.com>
2011-07-25 15:56:02 +0200

    init

---
+++ lib/ServerControl/Extension/RunAfter.pm
@@ -0,0 +1,27 @@
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package ServerControl::Extension::RunAfter
+
+use strict;
+use warnings;
+
+our $VERSION = '0.1.0';
+
+use ServerControl::Extension;
+use Data::Dumper;
+
+use base qw(ServerControl::Extension);
+
+__PACKAGE__->register('after_start', sub { shift->after_start(@_); });
+
+
+sub after_start {
+   my ($class, $sc) = @_;
+
+   my $args = ServerControl::Args->get;
+   system($args->{"run-after-start"});
+
+}
+
+
